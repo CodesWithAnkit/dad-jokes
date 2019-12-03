def60412b17cdacc3e8e5a0db7a4b6da69de6df2
Ankit Sharma <ankit.sharma@storynes.com>
2019-12-03 14:38:36 +0530

    Dad's jokes app in reactJs

---
+++ src/JokeList.js
@@ -0,0 +1,70 @@
+import React, { Component } from 'react';
+import Joke from './Joke';
+import axios from 'axios';
+import uuid from 'uuid/v4';
+import './JokeList.css';
+
+class JokeList extends Component {
+  static defaultProps = {
+    numJokesToGet: 10
+  };
+
+  constructor(props) {
+    super(props);
+    this.state = { jokes: [] };
+  }
+  async componentDidMount() {
+    // Loads Jokes
+    let jokes = [];
+
+    while (jokes.length < this.props.numJokesToGet) {
+      let res = await axios.get('https://icanhazdadjoke.com/', {
+        headers: { Accept: 'application/json' }
+      });
+      jokes.push({ id: uuid(), text: res.data.joke, votes: 0 });
+    }
+    this.setState({
+      jokes: jokes
+    });
+  }
+
+  //   Handling the vote
+  handleVote(id, delta) {
+    this.setState(st => ({
+      jokes: st.jokes.map(j =>
+        j.id === id ? { ...j, votes: j.votes + delta } : j
+      )
+    }));
+  }
+
+  render() {
+    return (
+      <div className='JokeList'>
+        <div className='JokeList-sidebar'>
+          <h1 className='Jokelist-title'>
+            <span>Dad</span> Jokes
+          </h1>
+          <img
+            alt='icon'
+            src='https://assets.dryicons.com/uploads/icon/svg/8927/0eb14c71-38f2-433a-bfc8-23d9c99b3647.svg'
+          />
+          <button className='JokeList-getmore'>New Jokes</button>
+        </div>
+
+        <div className='JokeList-jokes'>
+          {this.state.jokes.map(j => (
+            <Joke
+              key={j.id}
+              votes={j.votes}
+              text={j.text}
+              upVote={() => this.handleVote(j.id, 1)}
+              downVote={() => this.handleVote(j.id, -1)}
+            />
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default JokeList;
